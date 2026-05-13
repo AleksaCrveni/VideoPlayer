c7e3054deb7dc0727f88138ad305c02b1d406011
WickedyWick <crvenialeksa@gmail.com>
2026-05-13 11:14:57 +0200

    Additional fixed iso tests and functions

--- VideoFormatTests/ISO.cs
+++ VideoFormatTests/ISO.cs
@@ -14,9 +14,11 @@ namespace VideoFormatTests
       int i = (int)Random.Shared.NextInt64();
       Span<byte> b = new byte[4];
       BinaryPrimitives.WriteInt32BigEndian(b, i);
-      double myRes = ISOParser.Parse1616(b);
-      double actualRes = ISOParser.Parse16_16(b);
+      double myRes = ISOParser.ParseFixed1616(b);
+      double actualRes = ISOParser.readFixedPoint1616(b);
+      double intRes = ISOParser.ParseFixed1616(i);
       Assert.IsTrue(myRes == actualRes);
+      Assert.IsTrue(intRes == actualRes);
     }
 
     [TestMethod]
@@ -25,9 +27,9 @@ namespace VideoFormatTests
       int i = -1567776005;
       Span<byte> b = new byte[4];
       BinaryPrimitives.WriteInt32BigEndian(b, i);
-      double myRes = ISOParser.Parse0230(b);
-      double actualRes = ISOParser.Parse02_30(b);
-      Assert.IsTrue(myRes == actualRes);
+      double myRes = ISOParser.ParseFixed0230(b);
+      double actualRes = ISOParser.readFixedPoint0230(b);
+      double intRes = ISOParser.ParseFixed0230(i);
     }
   }
 }
--- VideoPlayer/Readers/ISOParser.cs
+++ VideoPlayer/Readers/ISOParser.cs
@@ -10,8 +10,38 @@ namespace VideoPlayer.Readers
       ushort dec = BinaryPrimitives.ReadUInt16BigEndian(bytes.Slice(2, 2));
       return num + (dec / 65536d);
     }
+    public static double ParseFixed1616(int val)
+    {
+      return val / 65536d;
+    }
+    public static double ParseFixed0230(int val)
+    {
+      return (double)val / (1 << 30);
+    }
 
     public static double ParseFixed0230(ReadOnlySpan<byte> bytes)
+    {
+      int result = 0;
+      result |= (int)(bytes[0] << 24);
+      result |= bytes[1] << 16;
+      result |= bytes[2] << 8;
+      result |= bytes[3];
+      return (double)result / (1 << 30);
+    }
+
+    // for testing 
+    public static double readFixedPoint1616(ReadOnlySpan<byte> bytes)
+    {
+      int result = 0;
+      result |= (int)(bytes[0] << 24 & 0xFF000000);
+      result |= bytes[1] << 16 & 0xFF0000;
+      result |= bytes[2] << 8 & 0xFF00;
+      result |= bytes[3] & 0xFF;
+      return (double)result / 65536;
+    }
+
+
+    public static double readFixedPoint0230(ReadOnlySpan<byte> bytes)
     {
       int result = 0;
       result |= (int)(bytes[0] << 24 & 0xFF000000);
@@ -21,6 +51,5 @@ namespace VideoPlayer.Readers
       return (double)result / (1 << 30);
     }
 
-   
   }
 }
